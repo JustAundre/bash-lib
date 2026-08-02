b056878cee901622ce6270f64ef2f84f16e280a5
JustAundre <126304209+JustAundre@users.noreply.github.com>
2026-08-01 19:37:37 -0500

    Removed `.sh` suffix.
    Fixed missing function exports.
    
    Fixed `find` wrapper not excluding paths which are children of the excluded paths.
    
    Added check to ensure paths don't already exist before making them in `perm_fix`.

--- pause.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-pause() {
-	local seconds="${1:-10}"
-	read -t"${seconds}" -n1 -rp "(Auto-resuming in ${seconds} seconds...)"$'\n⏸️ ⏸️ ⏸️ ===> ANY TO RESUME <=== ⏸️ ⏸️ ⏸️\n'
-	log i 'Resumed.'
-}
